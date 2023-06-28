043528a1ae471bfb31afc655bfeb3bf3e5b1b24e
abhishek peiris <abhishekdananjaya1@yahoo.com>
2023-06-28 23:36:32 +0530

    initial commit

---
+++ client_b/src/main/java/com/abhipeiris/spring_cloud_config/client_b/ClientBApplication.java
@@ -0,0 +1,13 @@
+package com.abhipeiris.spring_cloud_config.client_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientBApplication.class, args);
+	}
+
+}
---
+++ client_b/src/test/java/com/abhipeiris/spring_cloud_config/client_b/ClientBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhipeiris.spring_cloud_config.client_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
